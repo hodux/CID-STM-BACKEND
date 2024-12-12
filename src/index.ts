8dab01c93999ba5a511d3ba417ca0f804dc34ffd
shaheem <shaheemshedid4@gmail.com>
2024-12-12 18:28:33 -0500

    https server done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express, { Request, Response } from 'express';
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import {UserService} from "./services/user.service";
@@ -7,15 +10,23 @@ import userRoutes from './routes/user.route';
 import authRoute from "./routes/auth.route";
 import cors from 'cors';
 import {DB_connection} from "./config/database.config";
+import * as http from "node:http";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+const httpPort = process.env.HTTP || 3000;
+const httpsPort = process.env.HTTPS || 3001;
+const keyPath = process.env.SSL_KEY_PATH as string;
+const certPath = process.env.SSL_CERT_PATH as string;
 const uri = process.env.MONGO_URI as string;
 
 app.use(express.json());
+const options = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath)
+};
 
 // Mongoose connection
 DB_connection(uri);
@@ -28,8 +39,14 @@ app.get('/', (req: Request, res: Response) => {
 app.use("/api", userRoutes);
 app.use("/api", authRoute);
 // Start server
-app.listen(port, () => {
-  console.log(`Server running at <http://localhost:${port}>`);
+https.createServer(options, app).listen(httpsPort, () => {
+  console.log(`Serveur HTTPS en écoute sur <https://localhost>:${httpsPort}`);
+});
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": `https://localhost:${httpsPort}${req.url}` });
+  res.end();
+}).listen(httpPort, () => {
+  console.log(`Serveur HTTP en écoute sur <http://localhost>:${httpPort}`);
 });
 // console.log(UserService.createNewUser("test3", "test3@test.com", "abc-123"));
 // console.log(UserService.deleteUser("6754e43eb522b8b842c2714d"))
