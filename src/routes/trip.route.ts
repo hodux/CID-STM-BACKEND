cc2636fc08f810ed3d4fd704c4f8e2a83d7ecd5d
GaelDPaquin <gaeldpaquin@gmail.com>
2024-12-16 20:51:25 -0500

    avancement swagger, prob d'authentification

--- src/routes/trip.route.ts
+++ src/routes/trip.route.ts
@@ -6,6 +6,80 @@ import {TripController} from "../controllers/trip.controller";
 
 const router = Router();
 const tripController = new TripController();
+
+//SWAGGER DOCUMENTATION: GET ALL TRIPS
+
+/**
+ * @swagger
+ * /api/trips:
+ *   get:
+ *     summary: Retrieve and show all trips, does not require any parameters. || WILL TAKE A LOT OF TIME
+ *     description: Retrieve a list of all trips related to STM transport
+ *     tags: [Trips]
+ *     responses:
+ *       200:
+ *         description: A list of all trips.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 29317787435
+ *                   trip_id:
+ *                     type: string
+ *                     example: 29317787
+ *                   route_id:
+ *                     type: string
+ *                     example: 523
+ *                   stop_sequence:
+ *                     type: integer
+ *                     example: 523
+ *                   arrival_time:
+ *                     type: string
+ *                     example: 1293719837
+ *                   departure_time:
+ *                     type: string
+ *                     example: 1293719837
+ *                   stop_id:
+ *                     type: string
+ *                     example: 1231321
+ *                   departure_occupancy_status:
+ *                     type: string
+ *                     example: EMPTY
+ *                    
+ */
 router.get('/trips', tripController.getAllTrips);
+
+/**
+ * @swagger
+ * /api/trips/maxSequences:
+ *   get:
+ *     summary: Retrieve and show the maxSequences of all trips, if applicable
+ *     description: Retrieve a list of all the maxSequences of trips related to STM transport
+ *     tags: [Trips]
+ *     responses:
+ *       200:
+ *         description: A list of all trip's max sequence.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   route_id:
+ *                     type: string
+ *                     example: 523
+ *                   stop_sequence:
+ *                     type: integer
+ *                     example: 22
+ *                   
+ *                    
+ */
 router.get('/trips/maxSequences', tripController.getMaxSequences);
+
 export default router;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -7,6 +7,51 @@ import { roleMiddleware } from '../middlewares/roles.middleware';
 const router = Router();
 const userController = new UserController();
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Retrieve and show all users, does not require any parameters.
+ *     description: Retrieve a list of all users having access to this api
+ *     tags: [Users]
+ *     security:
+*         - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of all users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     example: 29317787435
+ *                   trip_id:
+ *                     type: string
+ *                     example: 29317787
+ *                   route_id:
+ *                     type: string
+ *                     example: 523
+ *                   stop_sequence:
+ *                     type: integer
+ *                     example: 523
+ *                   arrival_time:
+ *                     type: string
+ *                     example: 1293719837
+ *                   departure_time:
+ *                     type: string
+ *                     example: 1293719837
+ *                   stop_id:
+ *                     type: string
+ *                     example: 1231321
+ *                   departure_occupancy_status:
+ *                     type: string
+ *                     example: EMPTY
+ *                    
+ */
 // @ts-ignore
 router.get('/users',verifyToken, roleMiddleware(allRole), userController.getAllUsers);
 router.post('/users', userController.createNewUser);
