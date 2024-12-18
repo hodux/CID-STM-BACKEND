951d1e8507bbb953e2e443929296298c2c548acf
hodux <hoduxs@gmail.com>
2024-12-18 18:05:22 -0500

    Re-enable danfojs

--- src/controllers/vehicle.controller.ts
+++ src/controllers/vehicle.controller.ts
@@ -22,9 +22,9 @@ export class VehicleController {
     public async getVehicleCountPerSeat(req : Request, res : Response) {
         /*
         const vehicles = await VehicleService.getVehicleCountPerSeat();
-        if (vehicles != null) {
-            res.status(404).json({"message" : "No vehicles found"});
-        }
+        // if (vehicles != null) {
+        //     res.status(404).json({"message" : "No vehicles found"});
+        // }
         res.status(200).json(vehicles);
         */
     }
--- src/services/vehicle.service.ts
+++ src/services/vehicle.service.ts
@@ -1,9 +1,8 @@
 import {Vehicle} from "../models/vehicle.model";
 import {logger} from "../utils/logger";
-/*
 import * as dfd from "danfojs-node";
 import { toJSON } from "danfojs-node"
-*/
+
 export class VehicleService {
 
     public static async getAllVehicles() {
@@ -25,25 +24,23 @@ export class VehicleService {
             logger.error("Vehicle Service: getVehicleById -", error);
         }
     }
-    
+
 
     public static async getVehicleCountPerSeat() {
-        /*
         try {
             const vehicles = await Vehicle.find().lean();
-
             // Danfo, get the count of vehicle per seat occupancy
             const df = new dfd.DataFrame(vehicles);
             const grouped = df.groupby(["vehicle_occupancy_status"]).count();
             const final_df = grouped.loc({ columns: ['vehicle_occupancy_status', 'id_count'] }).resetIndex();
             final_df.rename({ 'id_count': 'count' }, { inplace: true });
             logger.info("Vehicle Service: getVehicleCountPerSeat - Success")
+            console.log(toJSON(final_df));
 
             return toJSON(final_df);
         } catch (error) {
             logger.error("Vehicle Service: getVehicleCountPerSeat - ", error);
         }
-            */
     }
 
 
